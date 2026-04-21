0db7f36b9ede341791666f305010ab1d37bf1faa
Haninhafez <abdohanin058@gmail.com>
2026-04-21 16:09:01 +0200

    reporistiry

---
+++ lib/feature/users/domain/repositories/user_repositories.dart
@@ -0,0 +1,7 @@
+import 'package:clean_code_app/core/errors/failuer.dart';
+import 'package:clean_code_app/feature/users/domain/entities/user_entities.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UserRepositories {
+  Future<Either<Failure, UserEntities>> getUsers();
+}
